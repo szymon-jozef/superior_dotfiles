30f7772a50c12670c960d8cae2f726854159559d
Szymon P <szymon_jozef@proton.me>
2026-01-16 19:52:40 +0100

    Update .config/btop/btop.conf Update .config/hypr/hyprland/binds/managment.conf Update .config/fish/fish_variables Update .config/fish/functions/open.fish

--- private_dot_config/private_fish/functions/open.fish
+++ private_dot_config/private_fish/functions/open.fish
@@ -1,3 +1,3 @@
 function open --wraps='nohup xdg-open  > /dev/null &' --description 'alias open=nohup xdg-open  > /dev/null &'
-    nohup xdg-open  > /dev/null & $argv
+    nohup xdg-open $argv > /dev/null & 
 end
