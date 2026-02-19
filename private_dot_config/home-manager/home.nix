4ea305701bb68bc393203e43d2d8fdc858626b47
Szymon P <szymon_jozef@proton.me>
2026-02-19 18:27:18 +0100

    Add .config/home-manager/home.nix

---
+++ private_dot_config/home-manager/home.nix
@@ -0,0 +1,111 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "szymon";
+  home.homeDirectory = "/home/szymon";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    btop
+    gh
+    fastfetch
+    neovim
+    yazi
+    fd
+    duf
+    gdu
+    tealdeer
+    gimp
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/szymon/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
+
+  home.sessionPath = [
+    "/home/szymon/.lmstudio/bin"
+  ];
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      # no dum greeting
+      set fish_greeting ""
+
+      # yes, please setup git for me
+      gh auth setup-git
+
+      # add github ssh key
+      # (dodałem przekierowanie błędów, żeby nie śmieciło na ekranie przy starcie)
+      eval (ssh-agent -c) > /dev/null
+      ssh-add ~/.ssh/github 2> /dev/null
+
+      # add github gpg key
+      set -gx GPG_TTY (tty)
+
+      # vi mode
+      fish_vi_key_bindings
+
+      # Odpalenie fastfetcha na start
+      clear
+      fastfetch --config ~/.config/fastfetch/startup.jsonc
+    '';
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
